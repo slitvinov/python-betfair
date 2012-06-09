6cb8d4ea5b775ed4ba851eb0d58d80be15408c43
Litvinov Sergey <slitvinov@gmail.com>
2012-06-09 18:28:44 +0200

    Fixed a bug in compid.sh

--- compid.sh
+++ compid.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+set -e
+set -u
+
 # compitition URL for Euro2012
 url=http://sports.betfair.com/football/competition?id=682772
 
@@ -7,14 +10,18 @@ url=http://sports.betfair.com/football/competition?id=682772
 WORK=work
 mkdir -p ${WORK}
 
-# fetch a web page
-wget ${url} -q -O ${WORK}/comp
+# fetch a web page if it is not in a cache
+if [ ! -r "${WORK}/comp" ]; then
+    wget ${url} -O ${WORK}/comp
+else
+    printf "using cache file: %s\n" ${WORK}/comp
+fi
 
 # get evnt IDs
-awk '/platformConfig = /{exit} 1' comp  | \
+awk '/platformConfig = /{exit} 1' ${WORK}/comp  | \
     grep 'data-eventid='  | \
     sed -e 's/\"//g' -e 's/data-eventid=//g' | \
     sort | uniq > ${WORK}/event.ids
 
-
+# run jparse.py with all IDs in event.ids
 xargs < ${WORK}/event.ids -n 1 ./jparse.py --id
