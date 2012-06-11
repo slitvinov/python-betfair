b7b4c2217ddd55dc36fa90522d3d8166bdb10e2c
Litvinov Sergey <slitvinov@gmail.com>
2012-06-11 09:07:19 +0200

    Fixing lint complins, add -n parameter for verbose output

--- jparse.py
+++ jparse.py
@@ -6,7 +6,7 @@ import re
 import json
 import urllib2
 
-# For the rule "Standard - Spiele", the following scores result:
+# For the rule "Standard - Spiele", the following SCORES result:
 #     Result: "2:1" - Bet: "1:2" -> 0 Points (wrong tendency)
 #     Result: "2:1" - Bet: "1:0" -> 2 Points (win goal difference)
 #     Result: "1:1" - Bet: "0:0" -> 1 Points (draw tendency)
@@ -38,42 +38,43 @@ def getexpwin(prob):
             pout = ""
             for res1 in xrange(4):
                 for res2 in xrange(4):
-                    np = npoint(res1, res2, bet1, bet2)
-                    p = prob[res1][res2]
-                    win = win + p*np
-                    if np>0 and len(pout)>0:
+                    number_point = npoint(res1, res2, bet1, bet2)
+                    probability = prob[res1][res2]
+                    win = win + probability*number_point
+                    if number_point > 0 and len(pout)>0:
                         pout = pout + "+"
-                    if np == 1:
-                        pout = pout + ("%.2f[%i %i]" % (p, res1, res2))
-                    elif np > 1:
-                        pout = pout + ("%i*%.2f[%i %i]" % (np, p, res1, res2))
+                    if number_point == 1:
+                        pout = pout + ("%.2f[%i %i]" % (probability, res1, res2))
+                    elif number_point > 1:
+                        pout = pout + ("%i*%.2f[%i %i]" % (number_point, probability, res1, res2))
             expwin.append( (win, bet1, bet2, pout) )
     return sorted(expwin)
 
-def walker(l):
+def walker(lst):
     """Collect odds from json tree"""
-    scoreset = {"0 - 0", "0 - 1", "0 - 2", "0 - 3",
+    SCORESET = {"0 - 0", "0 - 1", "0 - 2", "0 - 3",
                 "1 - 0", "1 - 1", "1 - 2", "1 - 3",
                 "2 - 0", "2 - 1", "2 - 2", "2 - 3",
                 "3 - 0", "3 - 1", "3 - 2", "3 - 3"}
     odds = [[0.0 for x in xrange(4)] for x in xrange(4)]
-    def walker_(l):
-        if type(l)==list:
-            map(walker_, l)
-        elif type(l)==dict:
-            if "marketType" in l:
-                if l["marketType"] == "CORRECT_SCORE":
-                    if "runners" in l:
-                        rlist=l["runners"]
-                        for runnerName in rlist:
-                            val = runnerName["runnerName"]
-                            if (val in scoreset) and ("prices" in runnerName):
-                                price=runnerName["prices"]["back"][0]["price"]
-                                res1=int(val[0])
-                                res2=int(val[4])
-                                odds[res1][res2]=price
-            [walker_(val) for val in l.values()]
-    walker_(l)
+    def walker_(lst):
+        if type(lst)==list:
+            [walker_(el) for el in lst]
+        elif type(lst)==dict:
+            if "marketType" in lst:
+                if lst["marketType"] == "CORRECT_SCORE":
+                    if "runners" in lst:
+                        rlist = lst["runners"]
+                        for runner_name in rlist:
+                            val = runner_name["runnerName"]
+                            if (val in SCORESET) and ("prices" in runner_name):
+                                price = runner_name["prices"]["back"][0]["price"]
+                                res1 = int(val[0])
+                                res2 = int(val[4])
+                                odds[res1][res2] = price
+            for val in lst.values():
+                walker_(val)
+    walker_(lst)
     return odds
 
 
@@ -104,33 +105,34 @@ def findkey(lst, key):
     findkey_(lst)
     return ret
 
-def finddict(l, key, val):
+def finddict(lst, key, val):
     """Returns a list of all values with a given key"""
     ret = []
-    def finddict_(l):
-        if type(l)==list:
-            map(finddict_, l)
-        elif type(l)==dict:
-            if key_and_val(l, key, val):
-                ret.append(l)
-            map(finddict_, l.values())
-    finddict_(l)
+    def finddict_(lst):
+        if type(lst)==list:
+            map(finddict_, lst)
+        elif type(lst)==dict:
+            if key_and_val(lst, key, val):
+                ret.append(lst)
+            map(finddict_, lst.values())
+    finddict_(lst)
     return ret
 
 def main():
-    Parser = OptionParser()
+    parser = OptionParser()
     # event ID Portugal - Niederlande
-    Parser.add_option("-i", "--id", dest="event_id",
-                      help="event ID, default: 26818281 (Portugal - Niederlande)",
+    parser.add_option("-i", "--id", dest="event_id",
+                      help="event ID, default: 26818281"
+                           " (Portugal v Netherlands)",
                       metavar="ID", default="26818281")
-    Parser.add_option("-n", "--number-show", dest="number_show",
+    parser.add_option("-n", "--number-show", dest="number_show",
                       type=int,
                       help="number of best results to show (default: 3)",
                       metavar="N", default=3)
 
-    Parser.add_option("-v", "--verbose",
+    parser.add_option("-v", "--verbose",
                       action="store_true", dest="verbose", default=False)
-    (options, args) = Parser.parse_args()
+    (options, args) = parser.parse_args()
 
     if options.verbose:
         logging.basicConfig(level=logging.DEBUG)
@@ -148,7 +150,7 @@ def main():
         f.close()
     else:
         if options.verbose:
-            logging.info("fetching data from url: %s" % url_with_tab)
+            logging.info("fetching data from url: %s", url_with_tab)
         # get file from the web
         opener = urllib2.build_opener()
         # TimeZone: Europe, Berlin
@@ -174,7 +176,7 @@ def main():
     json_string = re.sub(";[^;]*$", "", json_string)
 
     json_data = json.loads(json_string)
-    json_file=os.path.join("work", "json-full." + options.event_id + ".js")
+    json_file = os.path.join("work", "json-full." + options.event_id + ".js")
     write_json(json_file, json_data)
 
     # keep only relevant part of the json
@@ -219,13 +221,14 @@ def main():
         print "====================="
 
     # sort and print result
-    print "<expected point number>, <bet score 1>, <bet score 2>"
     wintable = getexpwin(prob)
 
     if options.verbose:
-        for t in wintable:
+        print "<expected point number>, <bet score 1>, <bet score 2>, <details>"
+        for t in wintable[-options.number_show:]:
             print "%.2f %i %i %s" % t
     else:
+        print "<expected point number>, <bet score 1>, <bet score 2>"
         for t in wintable[-options.number_show:]:
             print "%.2f %i %i" % t[0:3]
 
