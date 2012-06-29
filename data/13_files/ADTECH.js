9f5b4283c9e146905fbd55a4be581273a7a04b9f
Litvinov Sergey <slitvinov@gmail.com>
2012-06-29 11:27:27 +0200

    Add 13.html

---
+++ data/13_files/ADTECH.js
@@ -0,0 +1,2 @@
+document.write('<a href="http://adserver.adtech.de/?adlink/1101/3712495/0/357/AdId=-3;BnId=0;itime=961804114;" target="_blank"><img src="http://aka-cdn-ns.adtech.de/images/ATCollapse.gif" border="0" alt="hier klicken" width="200" height="600"/></a>');
+  
---
+++ data/13_files/ADTECH_002.js
@@ -0,0 +1,9 @@
+admeld_publisher  = 494;
+admeld_site       = 'mediasports';
+admeld_size       = '728x90';
+admeld_placement   = 'kicktipp';
+document.write("\n");
+document.write("<scr"+"ipt type=\"text/javascript\" src=\"http://js.admeld.com/meld128.js\">\n");
+document.write("</scr"+"ipt>\n");
+
+
