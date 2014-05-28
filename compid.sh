5969505de2e1fc8641a15b594699c77f05cd4631
LItvinov Sergey <Litvinov Sergey>
2014-05-28 23:32:28 +0200

    Restore `wget'.

--- compid.sh
+++ compid.sh
@@ -8,21 +8,21 @@ url=http://www.betfair.com/exchange/football/competition?id=67646
 
 # make a directory for intermediate files
 WORKDIR=work
-mkdir -p ${WORKDIR}
+mkdir -p "${WORKDIR}"
 
 # fetch a web page if it is not in a cache
 if [ ! -r "${WORKDIR}/comp" ]; then
-    ftp ${url} -o ${WORKDIR}/comp
+    wget ${url} -O "${WORKDIR}/comp"
 else
-    printf "using cache file: %s\n" ${WORKDIR}/comp
+    printf "using cache file: %s\n" "${WORKDIR}/comp"
 fi
 
 # get event IDs
-awk '/platformConfig = /{exit} 1' ${WORKDIR}/comp  | \
+awk '/platformConfig = /{exit} 1' "${WORKDIR}/comp"  | \
     grep 'data-eventid='  | \
     sed -e 's/.*data-eventid=\"//g' | \
     sed -e 's/\".*//g' | \
-    awk '!s[$0]++'  > ${WORKDIR}/event.ids
+    awk '!s[$0]++'  > "${WORKDIR}/event.ids"
 
 # run jparse.py with all IDs in event.ids
-xargs < ${WORKDIR}/event.ids -n 1 python2.7 ./jparse.py  -r cse --id
+xargs < "${WORKDIR}/event.ids" -n 1 python2.7 ./jparse.py  -r cse --id
